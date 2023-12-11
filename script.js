d870052264b5408299574842f7456d6ccf67a21e
Buyankhishig <114455198+buyankhishig23@users.noreply.github.com>
2023-12-11 20:16:56 +0800

    a

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let currentImageIndex = 0;
-    const images = document.querySelectorAll("#carousel .carousel-image");
-    const totalImages = images.length;
+const buttonHeight = 50;
+const buttonWidth = 150;
 
-    setInterval(function() {
-        images[currentImageIndex].classList.remove("active");
-        currentImageIndex = (currentImageIndex + 1) % totalImages;
-        images[currentImageIndex].classList.add("active");
-    }, 3000); 
+const maxWidth = window.innerWidth - buttonWidth;
+const maxHeight = window.innerHeight - buttonHeight;
+
+window.addEventListener('DOMContentLoaded', () => {
+    const nobutton = document.getElementById('nobutton');
+    const yesbutton = document.getElementById('yesbutton');
+
+    yesbutton.addEventListener('click', () => alert('Зөвшөөрсөнд баярлалаа <3 ⸜(｡˃ ᵕ ˂ )⸝'));
+
+    nobutton.addEventListener('mouseover', () => {
+        nobutton.style.left = Math.floor(Math.random() * (maxWidth + 1)) + 'px';
+        nobutton.style.top = Math.floor(Math.random() * (maxHeight + 1)) + 'px';
+    });
 });
